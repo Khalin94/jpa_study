d604fd0855a9ab568b9481b901c608bc279ab8fd
Khalin94 <mpzx1102@gmail.com>
2022-03-08 00:42:56 +0900

    1차 캐시 및 동일성 보장

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -22,55 +22,17 @@ public class JpaMain {
         tx.begin();
 
         try {
-           /*
-            트랜젝션 이용시 꼭 begin()을 해주자!
-            tx.begin();
-            Member member = new Member(1L, "helloA");
-            // member에 저장 (persist)
-            em.persist(member);
-            */
+            Member memberA = new Member(10L, "MemberA");
+            em.persist(memberA); // 영속성 컨텍스트에 먼저 저장한다..
 
-            /*
-            // 비영속(new)
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println("find member : " + findMember.getName());
+            // 1차 캐시를 사용하여 데이터를 가지고 오기 때문에 findMemberA와 findMemberA2는 같다 (findMemberA == findMemberB)
+            // 같은 id로 가지고 온 데이터에 대한 동일성이 보장된다.
+            Member findMemberA = em.find(Member.class, 10L);
+            Member findMemberA2 = em.find(Member.class, 10L);
 
-            // 영속(persist)
-            Member member = new Member(10L, "newMember");
-            em.persist(member);
+            System.out.println("is true? " + (findMemberA == findMemberA2));
 
-            // 준영속(detach)
-            em.detach(member);
-
-            // 삭제(remove)
-            em.remove(member);
-            */
-
-            /**
-             * JPQL
-             * jpql은 객체지향적으로 쿼리를 만든다. (객체지향 쿼리 SQL)
-             * jpql은 엔티티를 대상으로 쿼리(Member.class), 테이블 대상 X
-             * */
-            List<Member> memberList = em.createQuery("select m from Member as m", Member.class)
-                    // 페이징을 처리할 때 많이 사용..
-                    // 이렇게 사용하면 db가 바뀌어도 해당 db에 맞춰서 쿼리를 만든다.
-                    // ex) oracle 이라면 rownum으로 만든다.
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
-
-            for (Member member : memberList) {
-                System.out.println(member.getName());
-            }
-
-            // 삭제
-            //em.remove(findMember);
-
-            // db 업데이트 시 굳이 persist()를 안써도 된다.
-            // 설계가 컬렉션을 다루는 것 처럼 되어 있다.
-//            findMember.setName("helloJPA");
-
-            tx.commit();
+            tx.commit(); // 커밋 시 insert 쿼리가 나간다.
         }catch (Exception e){
             tx.rollback();
         }finally {
