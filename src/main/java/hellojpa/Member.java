ddf5a8119449b2edd2156d40910bb0d41a9a3cb2
Khalin94 <mpzx1102@gmail.com>
2022-03-11 00:32:47 +0900

    기본키 매핑 - TableGenerator (시퀀스 테이블 매핑)

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,21 +4,15 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR", // SequenceGenerator 이름
-        sequenceName = "MEMBER_SEQ", // 매핑할 시퀀스 이름
-        initialValue = 1, allocationSize = 1
-)
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ", allocationSize = 1
+) // sequence mapping 테이블을 만들 수 있다.
 public class Member {
 
-    // GeneratedValue가 없으면 id를 직접 할당해야됨.
-    //@GeneratedValue(strategy = GenerationType.AUTO)  // GenerationType.AUTO는 db에 맞게 auto_increase, sequence 등등 맞는 인덱스를 넣어준다.
     @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increase h2 2.x 버전에서 버그가 있어서 자동으로 생성이 안된다...
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE) // sequenceGenerator를 주지 않으면 시퀀스가 자동생성 된다. (이름을 정할 수 없다.)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-            generator = "MEMBER_SEQ_GENERATOR" // SequenceGenerator(매핑) 이름을 준다.
-    )
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR") // table sequence
     private Long id;
 
     private String name;
