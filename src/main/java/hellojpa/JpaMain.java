52f73b14af713fe7f6e48867a28932f0411705cd
Khalin94 <mpzx1102@gmail.com>
2022-03-21 01:52:58 +0900

    연관관계 기초 - 객체로 연관관계를 맺은 경우(ManyToOne, JoinColumn :: 객체지향적으로 활용 가능)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -23,19 +23,33 @@ public class JpaMain {
 
         try {
 
+            System.out.println("start >> ");
+
+            //저장
             Team team = new Team();
             team.setName("teamA");
             em.persist(team); // persist()를 하면 영속성 컨텍스트에서 관리하여 pk를 만드므로 밑에서 처럼 바로 사용가능하다.
 
             Member member = new Member();
             member.setName("member1");
-            member.setTeamId(team.getId());
+            member.setTeam(team);
             em.persist(member);
 
             // 테이블을 기준으로 객체를 생성하면 해당 객체를 가지고오기위해 외래키를 가지고와 찾아야한다.(객체 자체를 참조하는 것이 아니다.)
+//            Member findMember = em.find(Member.class, member.getId());
+//            Long teamId = findMember.getTeamId();
+//            Team findTeam = em.find(Team.class, teamId);
+
+
+            //연관관계를 통해 객체를 가지고 오는경우(객체지향적이다.)
             Member findMember = em.find(Member.class, member.getId());
-            Long teamId = findMember.getTeamId();
-            Team findTeam = em.find(Team.class, teamId);
+            Team findTeam = findMember.getTeam();
+
+            System.out.println("team :: " + findTeam.toString());
+
+            // 아래처럼 setter만 만들고 commit()을 날려주면 jpa에서 update를 자동으로 날려준다.(컬렉션처럼 사용함.)
+            findTeam.setName("dddd");
+            findMember.setTeam(findTeam);
 
 
 
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,15 +4,11 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@TableGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        table = "MY_SEQUENCES",
-        pkColumnValue = "MEMBER_SEQ", allocationSize = 1
-) // sequence mapping 테이블을 만들 수 있다.
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR") // table sequence
+//    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR") // table sequence
+    @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
 
@@ -20,8 +16,13 @@ public class Member {
     private String name;
 
     // 테이블에 맞춰 외래키 자체를 가지고 오는 경우 (객체지향적이지 못하다)
-    @Column(name = "TEAM_ID")
-    private Long teamId;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    // 연관관계를 통해 데이터를 가지고 오는경우(객체 자체를 사용)
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -39,11 +40,11 @@ public class Member {
         this.name = name;
     }
 
-    public Long getTeamId() {
-        return teamId;
+    public Team getTeam() {
+        return team;
     }
 
-    public void setTeamId(Long teamId) {
-        this.teamId = teamId;
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
